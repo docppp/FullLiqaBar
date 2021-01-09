74779ea25a5746b9cf1a6c52f9d2d257bb4eaf71
docppp <29142757+docppp@users.noreply.github.com>
2021-01-09 15:10:11 +0100

    Lowercase b in djangobar part 1.

--- djangobarwtf/asgi.py
+++ djangobarwtf/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobar.settings')
 
 application = get_asgi_application()
--- djangobarwtf/wsgi.py
+++ djangobarwtf/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobar.settings')
 
 application = get_wsgi_application()
